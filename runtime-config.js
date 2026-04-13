967dd7bd13644723f84cee6f1f61de815c77f82b
André Morais <andremorais.analytics@gmail.com>
2026-04-13 17:18:42 -0300

    Add files via upload

--- runtime-config.js
+++ runtime-config.js
@@ -1,6 +1,6 @@
 // Runtime configuration for static hosting (GitHub Pages, Cloudflare Pages, etc).
-// Update FORM_API_URL after deploying your Cloudflare Worker.
 window.NEXUS_RUNTIME = {
-  FORM_API_URL: "https://REPLACE_WITH_YOUR_WORKER.workers.dev"
+  FORM_API_URL:   "https://script.google.com/macros/s/AKfycbyTrjAcujgzLCSmJaWPJjUbPobFaKePSBPwnjKCIvrabNUN9Ezzj01L6vvWaTh1IN_s/exec",
+  SECRET_TOKEN:   "275c555c81666780aa3a995b1aa24f9c0bb2bda3"
 };
-
+ 
